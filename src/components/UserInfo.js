2e70d0406a67d4cc96f5d9b115a0b2822237642b
Holden Grissett <holdeng96@gmail.com>
2022-10-27 16:02:47 -0700

    MVP UserInfo finished
    Signed-off-by: Holden Grissett <holdeng96@gmail.com>

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function UserInfo() {
 
-    
+    let name = 'Nathan Fielder'
+
+    let highlightedReview = 'Tried the latest professor for 35L. He makes some GREAT jokes in-class.'
     return (
-        <div>UserInfo test!</div>
+        <div><div className='name'>{name}</div>
+        <br></br>
+        <div className='firstReview'>{highlightedReview}</div>
+        </div>
     )
 }
