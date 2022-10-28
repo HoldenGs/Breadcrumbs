8c6523abd8a527c2360745154595427075c387c6
Enemily <92351379+Enemily@users.noreply.github.com>
2022-10-28 02:20:23 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react"
-import {createRoot} from "react-dom/client"
+import ReactDOM from "react-dom"
+import App from "./components/App"
+import "bootstrap/dist/css/bootstrap.min.css"
 
-import App from "./App"
-
-import "./scss/style.scss"
-
-const root = createRoot(document.getElementById("root"))
-root.render(<App />)
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+)
