fde57bbe984d727e73dfa19c089b6621d96cbc72
Enemily <92351379+Enemily@users.noreply.github.com>
2022-10-29 17:57:14 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react"
-import Form from "./form"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Profile from "./components/profile"
+import UpdateProfile from "./components/updateProfile"
+import { AuthProvider } from "./components/AuthContext"
+
+
 function App() {
   return (
-<Form>
-
-</Form>
-
+      <div className="w-100" style={{ maxWidth: "800px" }}>
+      
+        <Router>
+          <AuthProvider>
+            <Routes>
+            <Route exact path="/" element={<Profile/>}  />
+            <Route  path="/profile" element={<Profile/>} />
+            <Route  path="/update-profile" element={<UpdateProfile/>} />
+            </Routes>
+          </AuthProvider>
+        </Router>
+      </div>
   )
 }
 
