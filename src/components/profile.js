e2faa057ab94cb3fd16e453cac76ac3d9a335e57
Enemily <92351379+Enemily@users.noreply.github.com>
2022-11-06 14:54:03 -0800

    Update profile.js

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,36 +1,12 @@
 import React, { useRef, useState } from "react"
-import UpdateProfile from "./updateProfile.js"
-import { Link, useHistory } from "react-router-dom"
 import "../scss/components/_profile.scss"
-import {nameFirst, nameLast, email, major, username, minor, bio} from './updateProfile'
+import {default as useAuth} from "./authContext"
+
 
 export default function Profile() {
-  //replace this with firestore data 
-  const [formData, setFormData] = React.useState(
-    {
-        firstName: "", 
-        lastName: "", 
-        email: "", 
-        year: "",
-        username: "",
-        password: "",
-        major: "",
-        minor: "",
-        bio: "",
-        finished: false,
-    }
-)
-    if(nameFirst!=formData.firstName) {
-      formData.firstName = nameFirst
-    }
 
   return ( <div className="div">
     <h2 className = "profile__h2"><a className = "profile__a" href="/update-profile"> Update Profile </a> <a className = "profile__a" href="/following"> {"   "}Following </a></h2>
-  <h1>{nameFirst} {nameLast}</h1>
-  <p class="title">{username}</p>
-  <p>{major}</p>
-  <p>{minor}</p>
-  <p>{bio}</p>
 </div>
   )
 }
