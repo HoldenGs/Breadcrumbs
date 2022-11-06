a5eb29a1805be9efe68a9f5cc14643a15850ab66
Enemily <92351379+Enemily@users.noreply.github.com>
2022-11-06 14:46:19 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from "react"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Profile from "./components/profile"
+import Profile from "./components/Profile"
 import UpdateProfile from "./components/updateProfile"
-import { AuthProvider } from "./components/AuthContext"
+import { AuthProvider } from "./components/authContext"
+import CreateProfile from "./components/createProfile"
+import Login from "./components/Login"
 
 
-function App() {
-  return (
-      <div className="w-100" style={{ maxWidth: "800px" }}>
-      
-        <Router>
-          <AuthProvider>
-            <Routes>
-            <Route exact path="/" element={<Profile/>}  />
-            <Route  path="/profile" element={<Profile/>} />
-            <Route  path="/update-profile" element={<UpdateProfile/>} />
-            </Routes>
-          </AuthProvider>
-        </Router>
-      </div>
-  )
+import {  createBrowserRouter,
+  RouterProvider,} from "react-router-dom"
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <AuthProvider><Login/></AuthProvider>,
+  },
+  {
+    path: "/profile",
+    element: <AuthProvider><Profile/></AuthProvider>,
+  },
+  {
+    path:"/create-profile",
+    element: <AuthProvider><CreateProfile/></AuthProvider>,
+  },
+  {
+    path:"/update-profile",
+    element: <AuthProvider><UpdateProfile/></AuthProvider>,
+  },
+
+])
+
+export default function App() {
+  return <RouterProvider router={router} />
 }
-
-export default App
