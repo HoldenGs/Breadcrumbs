2001295ef1005270650b23111c01731046b29b25
Ollie Pai <ollie.pai2003@gmail.com>
2022-11-05 18:59:54 -0700

    feat: install Mantine component library and implement working Select component

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Select } from '@mantine/core';
 
 export default function CourseCard({ courseName }) {
   return (
     <div className='course-card'>
       <div>{courseName}</div>
+      <Select
+        label="Your favorite framework/library"
+        placeholder="Pick one"
+        data={[
+          { value: 'react', label: 'React' },
+          { value: 'ng', label: 'Angular' },
+          { value: 'svelte', label: 'Svelte' },
+          { value: 'vue', label: 'Vue' },
+        ]}
+      ></Select>
+      <textarea rows='4' cols='36'></textarea>
     </div>
   )
 }
