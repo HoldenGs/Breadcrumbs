85eb9f3838368d9a2cf9152a3f883bdb55d10ff7
Enemily <92351379+Enemily@users.noreply.github.com>
2022-11-06 14:47:50 -0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,18 @@
-import firebase from "firebase/compat/app"
-import "firebase/compat/auth"
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
-const app = firebase.initializeApp({
+// Your web app's Firebase configuration
+const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
-})
-
-export const auth = app.auth()
-export default app
+   appId: process.env.REACT_APP_FIREBASE_APP_ID
+  };
+  // Initialize Firebase
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+export  {db, auth};
