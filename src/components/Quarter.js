deb207b02c17d0b551cb1e7604c13e2af0e1bc91
Ollie Pai <ollie.pai2003@gmail.com>
2022-11-09 23:55:37 -0800

    feat: dynamically load CourseCards according to data

--- src/components/Quarter.js
+++ src/components/Quarter.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import CourseCard from "./CourseCard"
 
 export default function Quarter({ quarterName }) {
+  let coursesTaken = ['COM SCI 31', 'COM SCI 32', 'COM SCI 33', 'COM SCI 35L']
   return (
     <div className="quarter">
       <div>{quarterName}</div>
+      {coursesTaken.length !== 0 ? (
+        coursesTaken.map((object, idx) => (
+          <CourseCard courseName={object}></CourseCard>
+        )) 
+      ) : (
+        <div>No courses taken this quarter.</div>
+      )}
     </div>
   )
 }
