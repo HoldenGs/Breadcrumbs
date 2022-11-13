1ce8ed0d562e738bff777807b888d6c4f233d1e3
Ollie Pai <ollie.pai2003@gmail.com>
2022-11-13 14:47:58 -0800

    feat: implement simple add course button

--- src/components/Quarter.js
+++ src/components/Quarter.js
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CourseCard from "./CourseCard"
 
 export default function Quarter({ quarterName }) {
-  let coursesTaken = ['COM SCI 31', 'COM SCI 32', 'COM SCI 33', 'COM SCI 35L']
+  const [coursesTaken, setCoursesTaken] = useState(['COM SCI 31', 'COM SCI 32', 'COM SCI 33', 'COM SCI 35L'])
+
+  function addCourseCard() {
+    let newCoursesTaken=coursesTaken.slice()
+    newCoursesTaken.push('New course')
+    setCoursesTaken(newCoursesTaken)
+  }
+  
   return (
     <div className="quarter">
       <div>{quarterName}</div>
       {coursesTaken.length !== 0 ? (
         coursesTaken.map((object, idx) => (
-          <CourseCard courseName={object}></CourseCard>
+          <CourseCard key={idx} courseName={object}></CourseCard>
         )) 
       ) : (
         <div>No courses taken this quarter.</div>
       )}
+      <button onClick={addCourseCard}>Add course</button>
     </div>
   )
 }
