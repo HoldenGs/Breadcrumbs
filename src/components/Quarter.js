fa2d63e60f3d0b36a253d494fe0986ff9b8cfb99
Ollie Pai <ollie.pai2003@gmail.com>
2022-11-13 15:18:04 -0800

    feat: use correct dummy data format to display CourseCard

--- src/components/Quarter.js
+++ src/components/Quarter.js
@@ -2,11 +2,20 @@ import React, { useState } from 'react'
 import CourseCard from "./CourseCard"
 
 export default function Quarter({ quarterName }) {
-  const [coursesTaken, setCoursesTaken] = useState(['COM SCI 31', 'COM SCI 32', 'COM SCI 33', 'COM SCI 35L'])
+  let dummy = {
+    courseCode: "COM SCI 35L",
+    courseTitle: "Software Construction",
+    professor: "Eggert",
+    qurater: "Fall 2022",
+    rating: 5,
+    feelings: "This class is hmmmmmm.",
+  }
+
+  const [coursesTaken, setCoursesTaken] = useState([])
 
   function addCourseCard() {
     let newCoursesTaken=coursesTaken.slice()
-    newCoursesTaken.push('New course')
+    newCoursesTaken.push(dummy)
     setCoursesTaken(newCoursesTaken)
   }
   
@@ -14,8 +23,8 @@ export default function Quarter({ quarterName }) {
     <div className="quarter">
       <div>{quarterName}</div>
       {coursesTaken.length !== 0 ? (
-        coursesTaken.map((object, idx) => (
-          <CourseCard key={idx} courseName={object}></CourseCard>
+        coursesTaken.map((course, idx) => (
+          <CourseCard key={idx} courseName={course.courseCode + " - " + course.courseTitle}></CourseCard>
         )) 
       ) : (
         <div>No courses taken this quarter.</div>
