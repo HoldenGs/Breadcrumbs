a9ade545777d1664af09fe83746f962d630548ae
Holden Grissett <holdeng96@gmail.com>
2022-11-18 16:35:19 -0800

    add prettierrc
    Signed-off-by: Holden Grissett <holdeng96@gmail.com>

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
@@ -14,5 +15,6 @@ const firebaseConfig = {
 
 const fbApp = initializeApp(firebaseConfig)
 const db = getFirestore(fbApp)
+const auth = getAuth(fbApp)
 
-export {fbApp, db}
+export {auth, db}
