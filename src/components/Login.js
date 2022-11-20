b0d59ab564b73f15fac9bc109181bf76d59f1705
Emily Nguyen <emilyngn@ucla.edu>
2022-11-19 19:21:51 -0800

    Removed the global variables (declared as let) that weren't used

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,4 @@
-
-import useAuth from './authContext'
+import useAuth from './AuthContext'
 import { Link, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 import { db } from '../firebase'
