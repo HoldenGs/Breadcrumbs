473026e935dc8d3851496704f4ce69a2f815b8eb
Enemily <92351379+Enemily@users.noreply.github.com>
2022-11-21 12:33:58 -0800

    added routes to following and profile pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-
+import { AuthProvider } from "./components/AuthContext";
 import Following from './pages/Following'
 import Login from './pages/Login'
 import Profile from './pages/Profile'
+import React from 'react'
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Following />,
-    errorElement: <div>Error!</div>
-  }
+	{
+		path: "/",
+		element: (
+			<AuthProvider>
+				<Login />
+			</AuthProvider>
+		),
+	},
+	{
+		path: "/profile/:username",
+		element: (
+			<AuthProvider>
+				<Profile/>
+			</AuthProvider>
+		),
+	  },
+	  {
+		path: "/following",
+		element: (
+			<AuthProvider>
+				<Following/>
+			</AuthProvider>
+		),
+	  },
 ])
 
 export default function App() {
