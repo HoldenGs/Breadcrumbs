ea24335ea1d04644f8a46a8f17bdd501eb1dc702
Enemily <92351379+Enemily@users.noreply.github.com>
2022-11-21 12:06:41 -0800

    integrated firebase authentication

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,29 @@
 import React, {useState} from 'react'
-
 import FullScreenContainer from '../components/FullScreenContainer'
 import TextInput from '../components/TextInput'
 import Button from '../components/Button'
+import useAuth from '../components/AuthContext'
+import { useNavigate } from 'react-router-dom'
+import { db } from '../firebase'
+import {
+	query,
+	getDocs,
+	collection,
+	where,
+	doc,
+	updateDoc,
+	serverTimestamp
+} from 'firebase/firestore'
 
+let userName = []
 export default function Login() {
   const [formData, setFormData] = useState({
     email: '',
     password: ''
   })
   const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
+	const { currentUser, login } = useAuth()
 
   function handleFormChange(e) {
     const {name, value} = e.target
@@ -19,9 +33,42 @@ export default function Login() {
     }))
   }
 
-  function handleLogin(e) {
+  async function handleLogin(e) {
     e.preventDefault()
     setLoading(true)
+    const userCredential = await login(formData.email, formData.password)
+		.catch(
+			(err) => {
+				console.log(err)
+			}
+		)
+		if (userCredential == null) {
+			console.log('error: no user login credential returned')
+			return
+		}
+
+		const userQuery = query(
+			collection(db, 'user'),
+			where('userID', '==', currentUser.uid.toString())
+		)
+		const snapshot = await getDocs(userQuery).catch((err) => {
+			console.log(err)
+		})
+		if (snapshot == null) {
+			console.log('error: no user login snapshot returned')
+			return
+		}
+
+		const userId = snapshot.docs[0].id
+		const loginUpdateRef = await doc(db, 'user', userId)
+		await updateDoc(loginUpdateRef, { loggedIn: serverTimestamp() }).catch(
+			(err) => {
+				console.log('Error updating user login timestamp: ', err)
+			}
+		)
+		
+		userName = snapshot.docs[0].data().username
+		navigate(`/profile/${userName}`, { state: snapshot.docs[0].data() })
   }
 
   return (
