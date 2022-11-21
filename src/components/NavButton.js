4151b3982fb2bfe96a475b92a098d36a8f88f8ad
Enemily <92351379+Enemily@users.noreply.github.com>
2022-11-21 12:27:47 -0800

    added username state

--- src/components/NavButton.js
+++ src/components/NavButton.js
@@ -2,10 +2,11 @@ import { NavLink } from "react-router-dom"
 
 // dest prop, navigation button destination
 // text prop, navigation button text
-export default function NavButton({ dest, text }) {
+export default function NavButton({ dest, text, userName }) {
 	return (
 		<NavLink
 			to={dest}
+			state={{username: userName}}
 			className='nav-button'
 		>
 			{text}
