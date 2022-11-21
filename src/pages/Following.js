02b02246b00fc726e8055a7b846188be2264e77e
Enemily <92351379+Enemily@users.noreply.github.com>
2022-11-21 12:12:46 -0800

    fetched "following" data from firestore

--- src/pages/Following.js
+++ src/pages/Following.js
@@ -1,33 +1,44 @@
 import React from 'react'
 import Header from '../components/Header'
 import ProfileCard from '../components/ProfileCard'
+import { useLocation} from "react-router-dom"
+import { db} from "../firebase"
+import { collection, getDocs, where, query } from "firebase/firestore"
 
 export default function Following() {
-  const usrs = [
-    {
-      name: 'Jason Tay',
-      year: '3rd Year',
-      major: 'Computer Science'
-    },
-    {
-      name: 'Ollie Pai',
-      year: '2nd Year',
-      major: 'Computer Science'
-    },
-    {
-      name: 'Sally Bo',
-      year: '4th Year',
-      major: 'Microbiology, Immunology, and Molecular Genetics'
-    },
-  ]
+  const [following, setFollowing] = useState([])
+  const location = useLocation()
+  const { username } = location.state
+
+  useEffect(() => {
+    setFollowing([])
+    const asyncFetchDailyData = async() => {
+      const followingIDs = []
+      const querySnapshot =  await getDocs(query(collection(db, "user"), where("username", "==", username)))
+      querySnapshot.forEach((doc) => {
+        followingIDs.push(doc.data().followers)
+        })
+        for(let i = 0; i<followingIDs[0].length; i++) {
+          const asyncFetchDailyData2 = async() => {
+            const querySnapshot =  await getDocs(query(collection(db, "user"), where("userID", "==", followingIDs[0][i])))
+            querySnapshot.forEach((doc) => {
+              setFollowing( arr => [...arr, doc.data()])
+            })
+          }
+          asyncFetchDailyData2()
+        }
+      }
+      asyncFetchDailyData()
+  }, [])
+  
   return (
     <div className='following'>
-      <Header />
-      {usrs.map((usr) => (
+      <Header username = {username}/>
+      {following.map((usr) => (
         <ProfileCard
-          name={usr.name}
+          name={usr.firstName + ' ' + usr.lastName}
           year={usr.year}
-          major={usr.major}
+          major={usr.major1}
         />
       ))}
       <ProfileCard />
