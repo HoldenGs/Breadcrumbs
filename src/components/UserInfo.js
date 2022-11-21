3658a01d8bfa0a2d25784fa3baed5b55e4075f10
Enemily <92351379+Enemily@users.noreply.github.com>
2022-11-21 12:17:27 -0800

    Update UserInfo.js

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,21 +1,78 @@
 import React, { useState, useEffect } from 'react'
 import TextInput from './TextInput'
 import { Select } from '@mantine/core'
+import useAuth from '../components/AuthContext'
+import { Link, useLocation } from 'react-router-dom'
+import { db } from '../firebase'
+import {
+	query,
+	getDocs,
+	collection,
+	where,
+	doc,
+	updateDoc,
+	arrayRemove, 
+	arrayUnion
+} from 'firebase/firestore'
 
-export default function UserInfo({name, year, major, editable}) {
+export default function UserInfo({username, name, year, major, editable}) {
   let years = new Map([['2023', "4th Year"], ['2024', "3rd Year"], ['2025', "2nd Year"], ['2026', "1st Year"]]);
 
+	const { currentUser } = useAuth()
   const [info, setInfo] = useState({
     firstName: 'Bobbie',
     lastName: 'Smith',
-    year: '2025',
-    major: 'Computer Science and Engineering'
+    gradYear: '2025',
+    major1: 'Computer Science and Engineering',
+    major2: '',
+    major3: '',
+    minor1: '',
+    minor2: '',
+    minor3: '',
+    userId: 0,
+    currentUsersFollowing: '',
+    userId: '',
+    username: ''
   })
+  const renderListMajor = listMajor(info.major1, info.major2, info.major3)
+  const renderListMinor = listMinor(info.minor1, info.minor2, info.minor3)
 
   useEffect(() => {
-    console.log(info)
-    console.log(years.get(info.year))
-  }, [info])
+    async function fetchData() {
+      const userQuery = query(collection(db, 'user'), where("username", "==", username))
+      const snapshot = await getDocs(userQuery).catch((err) => { console.log(err) })
+      if (snapshot == null) { console.log("error: no user login snapshot returned"); return }
+
+      const currentUser = snapshot.docs[0].data()
+      setInfo(prevFormData => ({
+        ...prevFormData,
+        firstName: currentUser.firstName,
+        lastName: currentUser.lastName,
+        gradYear: currentUser.year,
+        major: currentUser.major1,
+        userId: currentUser.userID,
+        username: currentUser.username,
+      }))  
+    }
+
+    if (info.userId === "") {
+      fetchData()
+    }
+
+    const asyncFetchCurrentUserFollowers = async() => {
+      const querySnapshot2 =  await getDocs(query(collection(db, "user"), where("userID", "==", currentUser.uid.toString())))
+        querySnapshot2.forEach((doc) => {
+        if(doc.data().followers.includes(info.userId.toString())) {
+          setInfo(prevFormData => ({
+            ...prevFormData,
+            currentUsersFollowing: true,
+          }))
+        }
+        });
+      }
+    asyncFetchCurrentUserFollowers();
+
+  }, [info.userId])
 
   function setProperty(event) {
     const {name, value} = event.target
@@ -24,6 +81,31 @@ export default function UserInfo({name, year, major, editable}) {
       [name]: value
     }))
   }
+  
+  function listMajor(major1, major2, major3) {
+		if (major2 === "") {
+			return <div className='user-info__name'>{`BS, ${major1}`}</div>
+		}
+		if (major2 !== "" && major3 === "") {
+			return <div className='user-info__name'>{`BS, ${major1}, BS, ${major2}`}</div>
+		}
+		return (
+			<div className='user-info__name'>{`BS, ${major1}, BS, ${major2}, BS, ${major3}`}</div>
+		)
+	}
+ 
+  function listMinor(minor1, minor2, minor3) {
+		if (minor1 === "") {
+			return
+		}
+		if (minor2 === "") {
+			return <div className='user-info__name'>{`Minor: ${minor1}`}</div>
+		}
+		if (minor2 !== "" && minor3 === "") {
+			return <div className='user-info__name'>{`Minor: ${minor1}, ${minor2}`}</div>
+		}
+		return <div className='user-info__name'>{`Minor: ${minor1}, ${minor2}, ${minor3}`}</div>
+	}
 
   return (
     <div className='user-info'>
@@ -68,7 +150,10 @@ export default function UserInfo({name, year, major, editable}) {
         <>
           <div className='user-info__name'>{info.firstName + ' ' + info.lastName}</div>
           <div className='user-info__year'>{years.get(info.year)}</div>
-          <div className='user-info__major'>{info.major}</div>
+          <div className='user-info__name'>{`@${info.username}`}</div>
+          {renderListMajor}
+          {renderListMinor}
+          <div className='user-info__year'>{'Graduation year: ' + info.gradYear}</div>
         </>
       )}
     </div>
