a8fd3af9f7678bc678f4d45ff24cc18743f142af
Emily Nguyen <emilyngn@ucla.edu>
2022-11-26 13:42:23 -0800

    added mantine multiselect feature to deal with selecting multiple majors/minors

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import TextInput from './TextInput'
-import { Select } from '@mantine/core'
+import { Select, MultiSelect } from '@mantine/core'
 import useAuth from '../components/AuthContext'
 import { db } from '../firebase'
 import {
@@ -12,44 +12,51 @@ import {
 	updateDoc,
 } from 'firebase/firestore'
 
-export default function UserInfo({username, name, year, major, editable}) {
-  let years = new Map([['2023', "4th Year"], ['2024', "3rd Year"], ['2025', "2nd Year"], ['2026', "1st Year"]]);
-
+export default function UserInfo({username, editable}) {
+  let years = new Map([['2023', "4th Year"], ['2024', "3rd Year"], ['2025', "2nd Year"], ['2026', "1st Year"]])
   const { currentUser } = useAuth()
   const [info, setInfo] = useState({
-    firstName: 'Bobbie',
-    lastName: 'Smith',
-    gradYear: '2025',
-    major1: 'Computer Science and Engineering',
-    major2: '',
-    major3: '',
-    minor1: '',
-    minor2: '',
-    minor3: '',
-    userId: '',
+    firstName: '',
+    lastName: '',
+    gradYear: '',
+    majors: [],
+    minors: [],
     currentUsersFollowing: '',
     userId: '',
     username: '',
   })
-  const renderListMajor = listMajor(info.major1, info.major2, info.major3)
-  const renderListMinor = listMinor(info.minor1, info.minor2, info.minor3)
-  const renderUpdateDoc = updateDocument()
+  const renderListMajor = listMajor(info.majors)
+  const renderListMinor = listMinor(info.minors)
+
+  //remove this eventually 
+  const majors = [
+    'Sociology',
+    'Computer Science and Engineering',
+    'Microbiology, Immunology, and Molecular Genetics',
+    'Independent Studies'
+  ]
+  const minors = [
+    'Sociology',
+    'Computer Science and Engineering',
+    'Microbiology, Immunology, and Molecular Genetics',
+    'Independent Studies'
+  ]
 
   useEffect(() => {
     async function fetchData() {
       const userQuery = query(collection(db, 'user'), where("username", "==", username))
       const snapshot = await getDocs(userQuery).catch((err) => { console.log(err) })
       if (snapshot == null) { console.log("error: no user login snapshot returned"); return }
-
       const currentUser = snapshot.docs[0].data()
       setInfo(prevFormData => ({
         ...prevFormData,
         firstName: currentUser.firstName,
         lastName: currentUser.lastName,
-        gradYear: currentUser.year,
-        major1: currentUser.major1,
+        gradYear: currentUser.gradYear,
         userId: currentUser.userID,
         username: currentUser.username,
+        majors: currentUser.majors,
+        minors: currentUser.minors,
       }))  
     }
 
@@ -78,50 +85,66 @@ export default function UserInfo({username, name, year, major, editable}) {
       ...prevFormData,
       [name]: value
     }))
+
+    const asyncUpdateInfo = async() => {
+      const querySnapshot2 =  await getDocs(query(collection(db, "user"), where("userID", "==", currentUser.uid.toString())))
+        querySnapshot2.forEach((docs) => {
+        const ref = doc(db, "user", docs.id)
+        updateDoc(ref, {
+          [name]: value
+          })
+        })
+      }
+      asyncUpdateInfo();
   }
+
+  function handleSelectChange(name, value) {
+    setInfo(prevFormData => ({
+      ...prevFormData,
+      [name]: value
+    }))
+
+    const asyncUpdateStudies = async() => {
+      const querySnapshot2 =  await getDocs(query(collection(db, "user"), where("userID", "==", currentUser.uid.toString())))
+        querySnapshot2.forEach((docs) => {
+          const ref = doc(db, "user", docs.id)
+          updateDoc(ref, {
+            [name]: value
+            })
+        })
+    }
+    asyncUpdateStudies()
+  }
+
   
-function listMajor(major1, major2, major3) {
-  if (major2 === "") {
-    return <div className='user-info__name'>{`BS, ${major1}`}</div>
+function listMajor(majors) {
+  if (typeof majors === 'undefined') {
+    return
   }
-  if (major2 !== "" && major3 === "") {
-    return <div className='user-info__name'>{`BS, ${major1}, BS, ${major2}`}</div>
+  if (majors.length == 1) {
+    return <div className='user-info__name'>{`BS, ${majors[0]}`}</div>
+  }
+  if (majors.length == 2) {
+    return <div className='user-info__name'>{`BS, ${majors[0]}, BS, ${majors[1]}`}</div>
   }
   return (
-    <div className='user-info__name'>{`BS, ${major1}, BS, ${major2}, BS, ${major3}`}</div>
+    <div className='user-info__name'>{`BS, ${majors[0]}, BS, ${majors[1]}, BS, ${majors[2]}`}</div>
   )
 }
 
-function listMinor(minor1, minor2, minor3) {
-  if (minor1 === "") {
+function listMinor(minors) {
+  if (typeof minors === 'undefined') {
     return
   }
-  if (minor2 === "") {
-    return <div className='user-info__name'>{`Minor: ${minor1}`}</div>
+  if (minors.length === 1) {
+    return <div className='user-info__name'>{`Minor: ${minors[0]}`}</div>
   }
-  if (minor2 !== "" && minor3 === "") {
-    return <div className='user-info__name'>{`Minor: ${minor1}, ${minor2}`}</div>
+  if (minors.length === 2) {
+    return <div className='user-info__name'>{`Minor: ${minors[0]}, ${minors[1]}`}</div>
   }
-  return <div className='user-info__name'>{`Minor: ${minor1}, ${minor2}, ${minor3}`}</div>
+  return <div className='user-info__name'>{`Minor: ${minors[0]}, ${minors[1]}, ${minors[2]}`}</div>
 }
 
-function updateDocument() {
-
-  const asyncFetchCurrentUserFollowers = async() => {
-    const querySnapshot2 =  await getDocs(query(collection(db, "user"), where("userID", "==", currentUser.uid.toString())))
-      querySnapshot2.forEach((docs) => {
-      const ref = doc(db, "user", docs.id)
-      updateDoc(ref, {
-          firstName: info.firstName,
-          lastName: info.lastName,
-          gradYear: info.gradYear,
-          major1: info.major1,
-        })
-      });
-    }
-  asyncFetchCurrentUserFollowers();
-
-}
 
   return (
     <div className='user-info'>
@@ -141,36 +164,49 @@ function updateDocument() {
             value={info.lastName}
             handleChange={setProperty}
           />
+          <TextInput
+            type='username'
+            name='username'
+            placeholder='username'
+            value={info.username}
+            handleChange={setProperty}
+          />
           <Select
             name='year'
             placeholder='Graduation Year'
             searchable
             value={info.year}
-            onChange={newVal => setProperty({
-              target: {
-                name: 'year',
-                value: newVal
-              }
-            })}
+            onChange={value => handleSelectChange('gradYear', value)}
             data={['2023', '2024', '2025', '2026']}
           />
-          <TextInput
-            type='major'
-            name='major1'
-            placeholder='Major'
-            value={info.major1}
-            handleChange={setProperty}
-          />
+          <MultiSelect
+          placeholder='Major'
+          searchable
+          nothingFound='Invalid Major'
+          data={majors}
+          value={info.majors}
+          onChange={value => handleSelectChange('majors', value)}
+          required
+          maxSelectedValues={3}
+        />
+        <MultiSelect
+          placeholder='Minor'
+          searchable
+          nothingFound='Invalid Minor'
+          data={minors}
+          value={info.minors}
+          onChange={value => handleSelectChange('minors', value)}
+          required
+          maxSelectedValues={3}
+        />
         </>
       ) : (
         <>
-          {renderUpdateDoc}
-          <div className='user-info__name'>{info.firstName + ' ' + info.lastName}</div>
-          <div className='user-info__year'>{years.get(info.year)}</div>
+          <div className='user-info__name'>{`${info.firstName} ${info.lastName}`}</div>
           <div className='user-info__name'>{`@${info.username}`}</div>
           {renderListMajor}
           {renderListMinor}
-          <div className='user-info__year'>{'Graduation year: ' + info.gradYear}</div>
+          <div className='user-info__year'>{years.get(info.gradYear)}</div>
         </>
       )}
     </div>
