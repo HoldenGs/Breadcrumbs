7cf33488e2eda46224575895cb82490bde74fb5d
Emily Nguyen <emilyngn@ucla.edu>
2022-11-29 12:18:31 -0800

    conditional rendering on course card

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import NavButton from './NavButton'
 
 import CourseCard from './CourseCard'
 
@@ -10,15 +9,20 @@ export default function ProfileCard({
 	reviewLabel,
 	review,
 }) {
+	const renderCourseCard = courseCard()
+	function courseCard() {
+		if (!review) return
+		return <CourseCard editable={false} reviewInfo={review} />
+	}
+
 	const years = ['1st Year', '2nd Year', '3rd Year', '4th Year']
-	console.log(review)
 	return (
 		<div className="profile-card">
 			<h2 className="profile-card__name">{name}</h2>
 			<h2 className="profile-card__year">{years[2026 - gradYear]}</h2>
 			<p className="profile-card__major">{major}</p>
 			<h3 className="profile-card__review-label">{reviewLabel}</h3>
-			<CourseCard editable={false} reviewInfo={review} />
+			{renderCourseCard}
 		</div>
 	)
 }
