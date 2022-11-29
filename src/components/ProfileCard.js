c89afd2ea8c8716943a61d9b6e61a735d456b565
Emily Nguyen <emilyngn@ucla.edu>
2022-11-28 17:48:24 -0800

    added NavLinks to usernames

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import NavButton from './NavButton'
 
 const YEARS = {
 	2026: '1st Year',
@@ -8,13 +9,16 @@ const YEARS = {
 }
 
 export default function ProfileCard({
-	name,
+	username,
+  id,
+  name,
 	gradYear,
 	major,
 	highlightedReview,
 }) {
 	return (
 		<div className="profile-card">
+      <NavButton dest={`/profile/${username}`} text={username} userID={id} />
 			<div className="profile-card__name">{name}</div>
 			<div className="profile-card__year">{YEARS[gradYear]}</div>
 			<div className="profile-card__major">{major}</div>
